0949c21639f93a389d324c9addb65479e1428a85
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-07-26 14:44:32 -0700

    Built inital MainScreen with Screen Builder

--- src/View_Controller/MainScreenController.java
+++ src/View_Controller/MainScreenController.java
@@ -1,4 +1,48 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class MainScreenController {
+    public Button SearchParts;
+    public TextField SearchTextParts;
+    public TableColumn PartID;
+    public TableColumn PartName;
+    public TableColumn InventoryLevelParts;
+    public TableColumn PriceParts;
+    public Button SearchProducts;
+    public TextField SearchTextProducts;
+    public TableColumn ProductID;
+    public TableColumn ProductName;
+    public TableColumn InventoryLevelProducts;
+    public TableColumn PriceProducts;
+
+    public void searchHandlerParts(ActionEvent actionEvent) {
+    }
+
+    public void addHandlerParts(ActionEvent actionEvent) {
+    }
+
+    public void modifyHandlerParts(ActionEvent actionEvent) {
+    }
+
+    public void deleteHandlerParts(ActionEvent actionEvent) {
+    }
+
+    public void searchHandlerProducts(ActionEvent actionEvent) {
+    }
+
+    public void addHandlerProducts(ActionEvent actionEvent) {
+    }
+
+    public void modifyHandlerProducts(ActionEvent actionEvent) {
+    }
+
+    public void deleteHandlerProducts(ActionEvent actionEvent) {
+    }
+
+    public void exitHandler(ActionEvent actionEvent) {
+    }
 }
