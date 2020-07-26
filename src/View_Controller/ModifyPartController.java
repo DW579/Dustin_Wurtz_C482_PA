991793675569cc84f64cbc88e42887ac0d73ce03
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-07-26 15:45:40 -0700

    Created modify part page for fxml and java

---
+++ src/View_Controller/ModifyPartController.java
@@ -0,0 +1,44 @@
+package View_Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
+public class ModifyPartController {
+    public RadioButton InHouse;
+    public RadioButton Outsourced;
+    public Label ID;
+    public Label Name;
+    public Label Inv;
+    public Label Price;
+    public Label Max;
+    public Label Min;
+    public Label CompanyName;
+
+    public void idHandler(ActionEvent actionEvent) {
+    }
+
+    public void nameHandler(ActionEvent actionEvent) {
+    }
+
+    public void invHandler(ActionEvent actionEvent) {
+    }
+
+    public void priceHandler(ActionEvent actionEvent) {
+    }
+
+    public void maxHandler(ActionEvent actionEvent) {
+    }
+
+    public void minHandler(ActionEvent actionEvent) {
+    }
+
+    public void companyHandler(ActionEvent actionEvent) {
+    }
+
+    public void saveHandler(ActionEvent actionEvent) {
+    }
+
+    public void cancelHandler(ActionEvent actionEvent) {
+    }
+}
