1a5edbf6d8272ff12536c51c84f3d4a016e4c074
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-07-26 16:18:32 -0700

    Created AddProduct through Scene Builder

--- src/View_Controller/AddProductController.java
+++ src/View_Controller/AddProductController.java
@@ -1,4 +1,59 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class AddProductController {
+    public Button Search;
+    public TextField SearchField;
+    public TableColumn PartIDAdd;
+    public TableColumn PartNameAdd;
+    public TableColumn InventoryLevelAdd;
+    public TableColumn PriceAdd;
+    public Label ID;
+    public Label Name;
+    public Label Inv;
+    public Label Price;
+    public Label Max;
+    public Label Min;
+    public TableColumn PartIDIncluded;
+    public TableColumn PartNameIncluded;
+    public TableColumn InventoryLevelIncluded;
+    public TableColumn PriceIncluded;
+
+    public void searchHandler(ActionEvent actionEvent) {
+    }
+
+    public void idHandler(ActionEvent actionEvent) {
+    }
+
+    public void nameHandler(ActionEvent actionEvent) {
+    }
+
+    public void invHandler(ActionEvent actionEvent) {
+    }
+
+    public void priceHandler(ActionEvent actionEvent) {
+    }
+
+    public void maxHandler(ActionEvent actionEvent) {
+    }
+
+    public void minHandler(ActionEvent actionEvent) {
+    }
+
+    public void addhandler(ActionEvent actionEvent) {
+    }
+
+    public void deleteHandler(ActionEvent actionEvent) {
+    }
+
+    public void cancelHandler(ActionEvent actionEvent) {
+    }
+
+    public void saveHandler(ActionEvent actionEvent) {
+    }
 }
