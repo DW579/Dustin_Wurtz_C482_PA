72b54711701b6d82dfbdd3f4f79f2a6bf4212134
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-07-26 15:30:08 -0700

    Built AddPart.fxml and AddPartController.java

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/MainScreen.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1224, 490));
         primaryStage.show();
     }
 
--- src/View_Controller/AddPartController.java
+++ src/View_Controller/AddPartController.java
@@ -1,4 +1,50 @@
 package View_Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+
 public class AddPartController {
+    public RadioButton InHouse;
+    public RadioButton Outsourced;
+    public Label ID;
+    public Label Name;
+    public Label Inv;
+    public Label Price;
+    public Label Max;
+    public Label Min;
+    public Label CompanyName;
+
+    public void inHouseHandler(ActionEvent actionEvent) {
+    }
+
+    public void outsourcedHandler(ActionEvent actionEvent) {
+    }
+
+    public void idHandler(ActionEvent actionEvent) {
+    }
+
+    public void nameHandler(ActionEvent actionEvent) {
+    }
+
+    public void invHandler(ActionEvent actionEvent) {
+    }
+
+    public void priceHandler(ActionEvent actionEvent) {
+    }
+
+    public void maxHandler(ActionEvent actionEvent) {
+    }
+
+    public void minHandler(ActionEvent actionEvent) {
+    }
+
+    public void companyNameHandler(ActionEvent actionEvent) {
+    }
+
+    public void saveHandler(ActionEvent actionEvent) {
+    }
+
+    public void cancelHandler(ActionEvent actionEvent) {
+    }
 }
