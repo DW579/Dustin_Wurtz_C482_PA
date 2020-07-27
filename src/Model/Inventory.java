84801db2bec30711c22cf6b7e5b5d7ad3f45a39a
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-07-27 08:46:06 -0700

    Created Model folder and included inital files for Part, Outsourced, InHouse, and Inventory

---
+++ src/Model/Inventory.java
@@ -0,0 +1,19 @@
+package Model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.Observable;
+
+public class Inventory {
+    private static ObservableList<Part> allParts = FXCollections.observableArrayList();
+//    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+
+    public static void addPart(Part newPart) {
+        allParts.add(newPart);
+    }
+
+    public static ObservableList<Part> getAllParts() {
+        return allParts;
+    }
+}
