bfac8ed505bac5bb3a09d8bcb604a7a1f23e9786
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-07-27 09:30:08 -0700

    Created initial Product.java

--- src/Model/Inventory.java
+++ src/Model/Inventory.java
@@ -7,7 +7,7 @@ import java.util.Observable;
 
 public class Inventory {
     private static ObservableList<Part> allParts = FXCollections.observableArrayList();
-//    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
+    private static ObservableList<Product> allProducts = FXCollections.observableArrayList();
 
     public static void addPart(Part newPart) {
         allParts.add(newPart);
@@ -15,5 +15,13 @@ public class Inventory {
 
     public static ObservableList<Part> getAllParts() {
         return allParts;
+    }
+
+    public static void addProduct(Product newProduct) {
+        allProducts.add(newProduct);
+    }
+
+    public static ObservableList<Product> getAllProducts() {
+        return allProducts;
     }
 }
