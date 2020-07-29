5cb9c8776931c982c873c35773ca670625a1075f
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-07-28 17:58:00 -0700

    Add either In House or out sourced part

--- src/View_Controller/AddPartController.java
+++ src/View_Controller/AddPartController.java
@@ -2,6 +2,7 @@ package View_Controller;
 
 //import com.sun.javafx.tk.quantum.SceneState;
 import Model.InHouse;
+import Model.Outsourced;
 import Model.Inventory;
 import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
@@ -17,6 +18,10 @@ public class AddPartController {
     private RadioButton InHouse;
     @FXML
     private RadioButton Outsourced;
+    @FXML
+    private Label SourceLabel;
+    @FXML
+    private TextField SourceField;
 
     public TextField IdField;
     public TextField NameField;
@@ -24,19 +29,32 @@ public class AddPartController {
     public TextField PriceField;
     public TextField MinField;
     public TextField MaxField;
-    public TextField CompanyNameField;
     public Button Cancel;
 
     private static int dynamicPartId = 0;
 
+    @FXML
+    private void initialize() {
+        InHouse.setSelected(false);
+        Outsourced.setSelected(true);
+    }
+
     public void inHouseHandler(ActionEvent actionEvent) {
+        // Switch radio buttons
         InHouse.setSelected(true);
         Outsourced.setSelected(false);
+
+        SourceLabel.setText("Machine ID");
+        SourceField.setPromptText("Mach ID");
     }
 
     public void outsourcedHandler(ActionEvent actionEvent) {
+        // Switch radio buttons
         InHouse.setSelected(false);
         Outsourced.setSelected(true);
+
+        SourceLabel.setText("Company Name");
+        SourceField.setPromptText("Comp Nm");
     }
 
     public void idHandler(ActionEvent actionEvent) {
@@ -49,8 +67,27 @@ public class AddPartController {
         int maxInt = Integer.parseInt(MaxField.getText());
         dynamicPartId += 1;
 
-        InHouse newPart = new InHouse(dynamicPartId, NameField.getText(), priceDouble, stockInt, minInt, maxInt, 0);
-        Inventory.addPart(newPart);
+        // If InHouse is true, else Outsourced is true
+        if(InHouse.isSelected()) {
+            // Set machine id
+            int machineId = Integer.parseInt(SourceField.getText());
+
+            // Create new part to add to inventory
+            InHouse newPart = new InHouse(dynamicPartId, NameField.getText(), priceDouble, stockInt, minInt, maxInt, machineId);
+
+            // Add new part to inventory
+            Inventory.addPart(newPart);
+        }
+        else {
+            // Set company name
+            String companyName = SourceField.getText();
+
+            // Create new part to add to inventory
+            Outsourced newPart = new Outsourced(dynamicPartId, NameField.getText(), priceDouble, stockInt, minInt, maxInt, companyName);
+
+            // Add new part to inventory
+            Inventory.addPart(newPart);
+        }
 
         // Close save window after save
         Stage stage = (Stage) Cancel.getScene().getWindow();
