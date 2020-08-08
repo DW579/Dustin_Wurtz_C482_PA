9de64e3eb83978b4cda5c4907deac0dd2939ed2d
Dustin Wurtz <dustinwurtz@Dustins-MacBook-Pro.local>
2020-08-07 20:55:45 -0700

    Data of selected product populating modify page

--- src/View_Controller/ModifyProductController.java
+++ src/View_Controller/ModifyProductController.java
@@ -1,6 +1,11 @@
 package View_Controller;
 
+import Model.Inventory;
+import Model.Part;
+import Model.Product;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -16,41 +21,44 @@ public class ModifyProductController {
     public TableColumn PartNameAdd;
     public TableColumn InventoryLevelAdd;
     public TableColumn PriceAdd;
-    public Label ID;
-    public Label Name;
-    public Label Inv;
-    public Label Price;
-    public Label Max;
-    public Label Min;
     public TableColumn PartIDIncluded;
     public TableColumn PartNameIncluded;
     public TableColumn InventoryLevelIncluded;
     public TableColumn PriceIncluded;
     public Button CancelButton;
 
+    // Selected product data
+    @FXML
+    private TextField IdField;
+    @FXML
+    private TextField NameField;
+    @FXML
+    private TextField InvField;
+    @FXML
+    private TextField PriceField;
+    @FXML
+    private TextField MaxField;
+    @FXML
+    private TextField MinField;
+
     public void selectedProduct(int id) throws IOException {
-        System.out.println(id);
+        System.out.println(Inventory.getAllProducts());
+        ObservableList<Product> allProdcutData = Inventory.getAllProducts();
+
+        allProdcutData.forEach(product -> {
+            if(product.getId() == id) {
+                // Insert data into text field depending on id selected
+                IdField.setText(Integer.toString(product.getId()));
+                NameField.setText(product.getName());
+                InvField.setText(Integer.toString(product.getStock()));
+                PriceField.setText(Double.toString(product.getPrice()));
+                MaxField.setText(Integer.toString(product.getMax()));
+                MinField.setText(Integer.toString(product.getMin()));
+            }
+        });
     }
 
     public void searchHandler(ActionEvent actionEvent) {
-    }
-
-    public void idHandler(ActionEvent actionEvent) {
-    }
-
-    public void nameHandler(ActionEvent actionEvent) {
-    }
-
-    public void invHandler(ActionEvent actionEvent) {
-    }
-
-    public void priceHandler(ActionEvent actionEvent) {
-    }
-
-    public void maxHandler(ActionEvent actionEvent) {
-    }
-
-    public void minHandler(ActionEvent actionEvent) {
     }
 
     public void addHandler(ActionEvent actionEvent) {
